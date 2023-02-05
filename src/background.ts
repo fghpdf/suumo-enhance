4c6b919b9a23c3d2a5aae56701dd9128ab43d7e2
fghpdf <fghpdf@outlook.com>
2023-02-05 22:15:36 +0900

    :sparkles: feat: get facilities from div

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: fghpdf
  * @Date: 2023-02-02 19:59:21
- * @LastEditTime: 2023-02-02 19:59:32
+ * @LastEditTime: 2023-02-05 22:12:06
  * @LastEditors: fghpdf
  */
 let active = false;
@@ -10,12 +10,31 @@ function makeOrange(color: string): void {
     document.body.style.backgroundColor = color;
 }
 
+function getFacilities(facilitiesDivId: string): string {
+    const facilitiesDiv = document.getElementById(facilitiesDivId);
+    if (!facilitiesDiv) {
+        console.warn(`div#${facilitiesDivId} can not be found`);
+        return '';
+    }
+    const facilitiesLis = facilitiesDiv.getElementsByTagName('li');
+    if (!facilitiesLis || facilitiesLis.length == 0) {
+        console.warn(`div#${facilitiesDivId} no li`);
+        return '';
+    }
+
+    const facilitiesStr = facilitiesLis[0].textContent;
+    if (!facilitiesStr) {
+        console.warn(`div#${facilitiesDivId} li no context`);
+        return '';
+    }
+    console.log(facilitiesStr);
+    return facilitiesStr;
+}
+
 chrome.action.onClicked.addListener((tab) => {
-    active = !active;
-    const color = active ? 'orange' : 'white';
     chrome.scripting.executeScript({
         target: {tabId: tab.id ? tab.id : -1},
-        func: makeOrange,
-        args: [color]
+        func: getFacilities,
+        args: ["bkdt-option"]
     }).then();
 });
