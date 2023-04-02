2f09ed3eabc6fe500a2b02daa59817ff307f3f40
fghpdf <fghpdf@outlook.com>
2023-04-02 16:02:04 +0900

    :lipstick: chore: add two icons

--- src/facilities.ts
+++ src/facilities.ts
@@ -1,13 +1,15 @@
 /*
  * @Author: fghpdf
  * @Date: 2023-03-01 22:02:03
- * @LastEditTime: 2023-03-01 22:33:17
+ * @LastEditTime: 2023-04-02 16:00:48
  * @LastEditors: fghpdf
  */
 const FACILITY_SVG_KEY: {[key: string]: string} = {
   "24時間ゴミ出し可": "24-garbage-room",
   "駐輪場": "parking-bicycle",
-  "耐震構造": "earthquake-resistant-structure"
+  "耐震構造": "earthquake-resistant-structure",
+  "バストイレ別": "dry-wet-separation",
+  "宅配ボックス": "delivery-box"
 };
 
 export default FACILITY_SVG_KEY;
