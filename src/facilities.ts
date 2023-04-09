7e4b74dcd38643e0242f234d9c5899ced6de8a7f
fghpdf <fghpdf@outlook.com>
2023-04-09 10:05:06 +0900

    :sparkles: feat: fiber optic

--- src/facilities.ts
+++ src/facilities.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: fghpdf
  * @Date: 2023-03-01 22:02:03
- * @LastEditTime: 2023-04-02 16:00:48
+ * @LastEditTime: 2023-04-09 10:04:39
  * @LastEditors: fghpdf
  */
 const FACILITY_SVG_KEY: {[key: string]: string} = {
@@ -9,7 +9,8 @@ const FACILITY_SVG_KEY: {[key: string]: string} = {
   "駐輪場": "parking-bicycle",
   "耐震構造": "earthquake-resistant-structure",
   "バストイレ別": "dry-wet-separation",
-  "宅配ボックス": "delivery-box"
+  "宅配ボックス": "delivery-box",
+  "光ファイバー": "fiber-optic"
 };
 
 export default FACILITY_SVG_KEY;
